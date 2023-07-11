11c64a7f580cd4b32880e8413ebecb500b526473
S1amyak <115889693+S1amyak@users.noreply.github.com>
2023-07-11 11:40:13 +0530

    pattern 14.cpp

--- pattern 14.cpp
+++ pattern 14.cpp
@@ -35,3 +35,24 @@ int main(){
   **
    *
 */
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int row = 1;
+    while(row <= n){
+        int space = 0;
+        while(space <= row - 1){
+            cout <<" ";
+            space ++;
+        }
+        int star = 1;
+        while(star <= n - row + 1){
+            cout << "*";
+            star ++;
+        }
+        cout << endl;
+        row ++;
+    }
+}
