ee096ec7b947337791aa9c13072cb0b4f36bc69b
S1amyak <115889693+S1amyak@users.noreply.github.com>
2023-08-02 22:52:56 +0530

    DAY 7

---
+++ time complexity.cpp
@@ -0,0 +1,9 @@
+/*
+time complexity
+constant time - O(1)        ---like in for loop 1 to 10 is range
+linear time - O(n)          ---like in for loop 1 to n is range
+logarithmic time - O(log n) --- binary search 
+quadratic time - O(n^2)     ---- 2 for loop with range 1 to n 
+cubic time - O(n^3)         --- 3 for loop with range 1 to n 
+*/
+
