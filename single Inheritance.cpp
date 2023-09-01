6dfb78c35d41da5581aee601268b1e7ac076b68e
S1amyak <115889693+S1amyak@users.noreply.github.com>
2023-09-01 20:55:38 +0530

    DAY 24

---
+++ single Inheritance.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+// single inheritance
+class Animal{
+    public:
+    int age;
+    int weight;
+
+    public:
+    void speak(){
+        cout << "Speaking" << endl;
+    }
+};
+
+class Dog: public Animal{
+
+};
+
+int main(){
+    Dog d;
+    d.speak();
+    cout <<d.age << endl; 
+}
